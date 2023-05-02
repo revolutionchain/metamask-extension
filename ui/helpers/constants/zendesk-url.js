b78a430cc53095846cb34e47e5d8b65f334bc528
Chase Ripplinger <chase@ripply.io>
2023-05-02 08:02:54 -0700

    Update zendesk urls with Qtum redirects

--- ui/helpers/constants/zendesk-url.js
+++ ui/helpers/constants/zendesk-url.js
@@ -1,41 +1,41 @@
 const ZENDESK_URLS = {
   ADD_CUSTOM_TOKENS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489031',
+    'https://www.qtum.org/help/add-custom-tokens',
   ADD_MISSING_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489271',
+    'https://www.qtum.org/help/add-missing-accounts',
   BASIC_SAFETY:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489591-Basic-Safety-Tips',
+    'https://www.qtum.org/help/basic-safety',
   CUSTOMIZE_NONCE:
-    'https://metamask.zendesk.com/hc/en-us/articles/7417499333531-How-to-customize-a-transaction-nonce',
+    'https://www.qtum.org/help/customize-nonce',
   HARDWARE_CONNECTION:
-    'https://metamask.zendesk.com/hc/en-us/articles/360020394612-How-to-connect-a-Trezor-or-Ledger-Hardware-Wallet',
+    'https://www.qtum.org/help/hardware-connection',
   IMPORT_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489331',
+    'https://www.qtum.org/help/import-accounts',
   IMPORTED_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015289932',
+    'https://www.qtum.org/help/imported-accounts',
   INFURA_BLOCKAGE:
-    'https://metamask.zendesk.com/hc/en-us/articles/360059386712',
-  LEGACY_WEB3: 'https://metamask.zendesk.com/hc/en-us/articles/360053147012',
+    'https://www.qtum.org/help/infura-blockage',
+  LEGACY_WEB3: 'https://www.qtum.org/help/legacy-web3',
   NFT_TOKENS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360058238591-NFT-tokens-in-MetaMask-wallet',
+    'https://www.qtum.org/help/nft-tokens',
   PASSWORD_ARTICLE:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404722782107',
+    'https://www.qtum.org/help/password-article',
   SECRET_RECOVERY_PHRASE:
-    'https://metamask.zendesk.com/hc/en-us/articles/360060826432-What-is-a-Secret-Recovery-Phrase-and-how-to-keep-your-crypto-wallet-secure',
+    'https://www.qtum.org/help/secret-recovery-phrase',
   SPEEDUP_CANCEL:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489251-How-to-speed-up-or-cancel-a-pending-transaction',
+    'https://www.qtum.org/help/speedup-cancel',
   TOKEN_SAFETY_PRACTICES:
-    'https://metamask.zendesk.com/hc/en-us/articles/4403988839451',
+    'https://www.qtum.org/help/token-safety-practices',
   UNKNOWN_NETWORK:
-    'https://metamask.zendesk.com/hc/en-us/articles/4417500466971',
+    'https://www.qtum.org/help/unknown-network',
   USER_GUIDE_CUSTOM_NETWORKS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404424659995',
+    'https://www.qtum.org/help/user-guide-custom-networks',
   USER_GUIDE_DAPPS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4405506066331-User-guide-Dapps',
+    'https://www.qtum.org/help/user-guide-dapps',
   USER_GUIDE_GAS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404600179227-User-Guide-Gas',
+    'https://www.qtum.org/help/user-guide-gas',
   VERIFY_CUSTOM_NETWORK:
-    'https://metamask.zendesk.com/hc/en-us/articles/360057142392',
+    'https://www.qtum.org/help/verify-custom-network',
 };
 
 export default ZENDESK_URLS;
